0cb96dd2e163b41a743d791b1b99aeb8bc362ae3
Nami Modarressi <s.namimodarressi@gmail.com>
2021-11-30 12:32:01 +0330

    Getting alphabet and String added

--- main.py
+++ main.py
@@ -3,4 +3,33 @@
 # Nov. 2021
 # Seyed Nami Modarressi
 
-print("Hello :)")
+# Todo :
+#  1 ) input String and machines
+#  2 ) write accept function
+#  3 ) use function for A and B and String 
+#  4 ) add an example
+
+alphabet = []
+automata_A = []
+automata_B = []
+input_String = ""
+
+
+alphabet = input("\n\nEnter alphabet with ',' (exp. :  a,b,c  or  0,1) :")
+alphabet = alphabet.split(",")
+input_String = input("\nEnter String :")
+in_alphabet_flag = 0
+for i in input_String:
+    for j in alphabet:
+        if i == j:
+            in_alphabet_flag = 1
+            break
+    if in_alphabet_flag == 1:
+        break
+if in_alphabet_flag == 0:
+    print("\nInvalid String . Insert String with given alphabet")
+else:
+    # String is OK
+    # now we get machines
+    print("\nOK String")
+
